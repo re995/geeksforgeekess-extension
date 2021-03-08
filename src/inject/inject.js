4dc2fe9618c953e5786f076b8ff57a2b31985727
Ron Even <ron.even@watchful.ai>
2021-03-08 02:00:14 +0200

    Intermediate commit

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,61 +4,65 @@ chrome.runtime.sendMessage({ operation: 'getDetailsPathInPage', url: currentUrl 
     // TODO: Check with stocks if need to be shown
     var show = true;
     if (show) {
-      showPopup()
+      showPopup(currentUrl);
   }
 });
 
+function getDomainFromUrl(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
 
-function getStockDetails(url) {
-  console.log("Requesting stock details for " + url);
-
-  // TODO: Make actual request...
-  /* var response = $.ajax(
-    {
-      method: 'GET',
-      url: 'https://api.backand.com/1/objects/action/Items/?name=getRelevant&parameters={"item":"' + model + '","price":"' + price + '"}',
-      headers:
-      {
-        'AnonymousToken': '7bd597d9-86ba-41d6-ab63-f498c59fb2b9'
-      }
-    }
-    );
-  */
-  console.log('getStockDetails request sent');
-
-  response.fail(function (e) {
-    console.error('ERROR getting stock details')
-    console.log(e);
-  });
-
-  response.success(function (result) {
-    if (result && result.length) {
-      console.log('Got stock details');
-      // TODO: Process request
-      popupProduct(cheapest, price);
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
     }
     else {
-      console.log('No response returned for getStockDetails request');
+        hostname = url.split('/')[0];
     }
-  });
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname.replace(".com", "").replace("www.", "");
 }
 
-function popupProduct(product, originalPrice) {
+function translateUrlToStockSymbol(url) {
+    var sitename = getDomainFromUrl(url)
+    console.log("SITENAME = " + sitename);
+    var response = $.ajax(
+    {
+      method: 'GET',
+      url: 'https://financialmodelingprep.com/api/v3/search?query=' + sitename + '&limit=10&exchange=NASDAQ&apikey=demo',
+      async: false
+    });
+
+    var responseObject = response.responseJSON;
+    return responseObject[0]['symbol'];
+
+}
+
+function showPopup(currentUrl) {
   var myid = chrome.runtime.id;
+  var stocksymbol = translateUrlToStockSymbol(currentUrl)
+  console.log(stocksymbol)
   // TODO: Make it ours :)
   var headToInject = '<script src="chrome-extension://' + myid + '/js/jquery/jquery.min.js"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/normalize.css"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/demo.css"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/menu_topexpand.css"/>' +
-    '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/buttons.css"/>';
+    '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/buttons.css"/>' +
+    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">';
 
   var scriptsToInject = '<script src="chrome-extension://' + myid + '/src/inject/js/classie.js"></script>' +
-    '<script src="chrome-extension://' + myid + '/src/inject/js/main.js"></script>';
+    '<script src="chrome-extension://' + myid + '/src/inject/js/main.js"></script>'
+    '<script>var stockSymbol="FB",link="https://api.stockdio.com/visualization/financial/charts/v1/PricesChange?app-key=01E29D3ADEC844F799CC7476C142A17B&symbol="+stockSymbol+"&palette=Financial-Light&showLogo=Title";$(document).ready(function(){$(".btn").click(function(){$("#myModal").modal("show"),$("#graph").attr("src",link)})}); </script>';
 
   var popupToInject = '<div style="background:#b4bad2" class="menu-wrap buytheway">' +
 				'<nav style="background:#b4bad2" class="menu buytheway sticky navbar navbar-default navbar-fixed-top" >' +
     '<div class="center-vertical buytheway" style="padding-right:2em">' +
-    '<span style="font-size:1.5em">מצאנו את המוצר זול ב- ' + (originalPrice - product.price) + '₪ ב' + product.name + '!</span>' +
+    '<span style="font-size:1.5em">SOME TEXT HERE</span>' +
+    "<iframe id='st_af7a75818bc7411cab50ef05fe0494a1' frameBorder='0' scrolling='no' width='100%' height='100%' src='https://api.stockdio.com/visualization/financial/charts/v1/Ticker?app-key=01E29D3ADEC844F799CC7476C142A17B&symbols=AAPL;MSFT;GOOG;FB;ORCL&palette=Financial-Light&layoutType=2&onload=st_af7a75818bc7411cab50ef05fe0494a1'></iframe>" +
     '</div>' +
     '<button id ="btn_newtab" class="buytheway center-vertical left button button--shikoba button--round-s button--border-thin button--size-l"><i class="button__icon icon"><img src="chrome-extension://' + myid + '/icons/newtabicon.png" /></i><span>תראה לי!</span></button>' +
 
@@ -68,10 +72,10 @@ function popupProduct(product, originalPrice) {
 
   setTimeout(function () {
     $('head').append(headToInject);
-    $('body').prepend('<h1>Hello World</h1>');
+    $('body').prepend(scriptsToInject);
     $('body').prepend(popupToInject);
 
-    setNewTabButtonTarget(product.url);
+    setNewTabButtonTarget(currentUrl);
   }, 2000);
 
 
