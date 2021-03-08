39a329a745a29ebec2b85b8847f372e9eb0cc08a
Ron Even <ron.even@watchful.ai>
2021-03-08 11:39:13 +0200

    Made fixes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,7 @@
 var currentUrl = window.location.href;
 chrome.runtime.sendMessage({ operation: 'getDetailsPathInPage', url: currentUrl }, function (response) {
     console.log('Got details in page');
-    // TODO: Check with stocks if need to be shown
-    var show = true;
-    if (show) {
-      showPopup(currentUrl);
-  }
+    tryShowPopup(currentUrl);
 });
 
 function getDomainFromUrl(url) {
@@ -30,59 +26,106 @@ function getDomainFromUrl(url) {
 function translateUrlToStockSymbol(url) {
     var sitename = getDomainFromUrl(url)
     console.log("SITENAME = " + sitename);
+    var requestUrl =  'https://financialmodelingprep.com/api/v3/search?query=' + sitename + '&limit=10&apikey=demo';
+    console.log("Requesting: " + requestUrl)
     var response = $.ajax(
     {
       method: 'GET',
-      url: 'https://financialmodelingprep.com/api/v3/search?query=' + sitename + '&limit=10&exchange=NASDAQ&apikey=demo',
+      url: requestUrl,
       async: false
     });
 
     var responseObject = response.responseJSON;
+    if (responseObject.length == 0) {
+        return null;
+    }
     return responseObject[0]['symbol'];
-
 }
 
-function showPopup(currentUrl) {
+function tryShowPopup(currentUrl) {
   var myid = chrome.runtime.id;
   var stocksymbol = translateUrlToStockSymbol(currentUrl)
-  console.log(stocksymbol)
+  if (stocksymbol === null) {
+        console.log("Couldn't find a matching stock. Not showing")
+        return;
+  }
+
+  console.log("Found stock symbol " + stocksymbol);
   // TODO: Make it ours :)
   var headToInject = '<script src="chrome-extension://' + myid + '/js/jquery/jquery.min.js"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/normalize.css"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/demo.css"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/menu_topexpand.css"/>' +
     '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/src/inject/css/buttons.css"/>' +
-    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">';
+    '<link rel="stylesheet" type="text/css" href="chrome-extension://' + myid + '/css/bootstrap.min.css"/>' ;
 
   var scriptsToInject = '<script src="chrome-extension://' + myid + '/src/inject/js/classie.js"></script>' +
-    '<script src="chrome-extension://' + myid + '/src/inject/js/main.js"></script>'
-    '<script>var stockSymbol="FB",link="https://api.stockdio.com/visualization/financial/charts/v1/PricesChange?app-key=01E29D3ADEC844F799CC7476C142A17B&symbol="+stockSymbol+"&palette=Financial-Light&showLogo=Title";$(document).ready(function(){$(".btn").click(function(){$("#myModal").modal("show"),$("#graph").attr("src",link)})}); </script>';
+    '<script src="chrome-extension://' + myid + '/src/inject/js/main.js"></script>' +
+    '<script src="chrome-extension://' + myid + '/js/bootstrap.min.js"></script>' +
+    '<script>var link="https://api.stockdio.com/visualization/financial/charts/v1/PricesChange?app-key=01E29D3ADEC844F799CC7476C142A17B&symbol=' + stocksymbol + '&palette=Financial-Light&showLogo=Title";$(document).ready(function(){$("#btn_newtab").click(function(){$("#myModal").modal("show"),$("#graph").attr("src",link)})}); </script>';
 
-  var popupToInject = '<div style="background:#b4bad2" class="menu-wrap buytheway">' +
-				'<nav style="background:#b4bad2" class="menu buytheway sticky navbar navbar-default navbar-fixed-top" >' +
+  var popupToInject = '<div style="background:inherit" class="menu-wrap buytheway">' +
+				'<nav style="background:inherit" class="menu buytheway sticky navbar navbar-default navbar-fixed-top" >' +
+        "<iframe id='st_af7a75818bc7411cab50ef05fe0494a1' frameBorder='0' scrolling='no' width='100%' height='100%' src='https://api.stockdio.com/visualization/financial/charts/v1/Ticker?app-key=01E29D3ADEC844F799CC7476C142A17B&symbols=" + stocksymbol + ";AAPL;MSFT;GOOG;FB;ORCL&palette=Financial-Dark&layoutType=2&onload=st_af7a75818bc7411cab50ef05fe0494a1'></iframe>" +
     '<div class="center-vertical buytheway" style="padding-right:2em">' +
-    '<span style="font-size:1.5em">SOME TEXT HERE</span>' +
-    "<iframe id='st_af7a75818bc7411cab50ef05fe0494a1' frameBorder='0' scrolling='no' width='100%' height='100%' src='https://api.stockdio.com/visualization/financial/charts/v1/Ticker?app-key=01E29D3ADEC844F799CC7476C142A17B&symbols=AAPL;MSFT;GOOG;FB;ORCL&palette=Financial-Light&layoutType=2&onload=st_af7a75818bc7411cab50ef05fe0494a1'></iframe>" +
+    '<span style="font-size:1.5em; text-align:center">SOME TEXT HERE</span>' +
     '</div>' +
-    '<button id ="btn_newtab" class="buytheway center-vertical left button button--shikoba button--round-s button--border-thin button--size-l"><i class="button__icon icon"><img src="chrome-extension://' + myid + '/icons/newtabicon.png" /></i><span>תראה לי!</span></button>' +
+    '<button id ="btn_newtab" class="buytheway center-vertical button button--shikoba button--round-s button--border-thin button--size-l"><i class="button__icon icon"><img src="chrome-extension://' + myid + '/icons/newtabicon.png" /></i><span>עולם ההשקעות קורא לך!</span></button>' +
 
     '<a href="#" id="btn_close" class="close-thin buytheway"></a>' +
 				'</nav>' +
-    '</div>';
+    '</div>' +
+    `
+    <div id="myModal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                  <iframe frameBorder='0' scrolling='no' width='600' height='320' id="graph" src='https://api.stockdio.com/visualization/financial/charts/v1/PricesChange?app-key=01E29D3ADEC844F799CC7476C142A17B&symbol=TSLA&palette=Financial-Light&showLogo=Title'></iframe>
+                  <div id='moreinfo_content'></div>
+                </div>
+            </div>  
+        </div>
+    </div>
+
+    `
+
+    ;
 
   setTimeout(function () {
     $('head').append(headToInject);
     $('body').prepend(scriptsToInject);
     $('body').prepend(popupToInject);
 
-    setNewTabButtonTarget(currentUrl);
+    setupMoreInfoPopup(stocksymbol);
   }, 2000);
 
 
 }
 
-function setNewTabButtonTarget(url) {
-  $("#btn_newtab").on('click', function () {
-    window.open(url);
-		});
+function setupMoreInfoPopup(stocksymbol) {
+  console.log("Setting button onclick for symbol " + stocksymbol)
+  var link = 'https://api.stockdio.com/visualization/financial/charts/v1/PricesChange?app-key=01E29D3ADEC844F799CC7476C142A17B&symbol='+stocksymbol+'&palette=Financial-Light&showLogo=Title'
+    $("#btn_newtab").click(function(){
+        $("#myModal").modal('show');
+        $("#graph").attr('src', link);
+    });
+
+  var extraContent = ""
+  var response = $.ajax(
+    {
+      method: 'GET',
+      url: 'http://127.0.0.1:5000/request',
+      headres: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      async: false
+    });
+
+    var responseText = response.responseText;
+    if (responseText.length == 0) {
+        console.error("Error while getting more info from server!")
+    }
+    
+    $("#moreinfo_content").html(responseText);
+
 }
+
